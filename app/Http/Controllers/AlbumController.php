73538fa767b19e648e41c4617f4eff91c5402537
jesus-eo <jesusmanuel.espinar@iesdonana.org>
2022-02-09 23:24:50 +0100

    Creado de tablas

---
+++ app/Http/Controllers/AlbumController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Requests\StoreAlbumRequest;
+use App\Http\Requests\UpdateAlbumRequest;
+use App\Models\Album;
+use Illuminate\Http\Request;
+
+class AlbumController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $albumes = Album::all();
+
+        return view('albumes.index', [
+            'albumes' => $albumes,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $album = new Album();
+
+        return view('albumes.create', [
+            'album' => $album
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validados = $request->validated();
+        $album = new Album($validados);
+        $album->save();
+        $request->file('portada')->storeAs(
+            'portadas',
+            $album->id . '.jpg',
+            'public',
+        );
+        return redirect()->route('albumes.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
